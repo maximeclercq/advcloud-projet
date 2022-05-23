9f1bc362b708b2474e8e5f1a7f6bf82fc5d76f67
Thibault Garrot <thibault.garrot@etu.unice.fr>
2022-05-23 14:25:27 +0200

    Test RabbitMQTT with Docker containers

--- src/broker_receiver.py
+++ src/broker_receiver.py
@@ -2,8 +2,11 @@ import pika
 import socket
 import struct
 import os.path
+import os
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+host = os.environ.get('AMQP_HOST')
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
 channel = connection.channel()
 
 channel.exchange_declare(exchange="logs", exchange_type="fanout")
@@ -18,22 +21,22 @@ print(" [*] Waiting for logs. To exit press CTRL+C")
 
 def callback(ch, method, properties, body):
     print("[x] %r" % body)
-    send_to_other_process(body)
+    #send_to_other_process(body)
 
 
-def send_to_other_process(body):
-    print("Connecting...")
-    if os.path.exists("/tmp/python_unix_sockets_example"):
-        client = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-        client.connect("/tmp/python_unix_sockets_example")
-        print("Ready.")
-        print("Ctrl-C to quit.")
-        print("SEND:", body)
-        client.send(body)
-        client.close()
-    else:
-        print("Couldn't Connect!")
-        print("Done")
+# def send_to_other_process(body):
+#     print("Connecting...")
+#     if os.path.exists("/tmp/python_unix_sockets_example"):
+#         client = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+#         client.connect("/tmp/python_unix_sockets_example")
+#         print("Ready.")
+#         print("Ctrl-C to quit.")
+#         print("SEND:", body)
+#         client.send(body)
+#         client.close()
+#     else:
+#         print("Couldn't Connect!")
+#         print("Done")
 
 
 channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
---
+++ src/emit.Dockerfile
@@ -0,0 +1,15 @@
+FROM ubuntu:20.04
+
+RUN apt update
+RUN apt install -y python3 python3-pip python3-dev build-essential gcc
+
+RUN pip3 install --upgrade pip
+
+RUN mkdir /app
+WORKDIR /app
+COPY . /app
+
+# RUN pip3 install -r requirements.txt
+RUN pip3 install pika
+
+CMD ["python3", "emit_log.py"]
